850e7b3937633ab7525cbf8bef3c46dd3f0d6da0
Rafi Antareza Putra <1108rap@gmail.com>
2021-04-18 15:15:51 +0700

    Configuration JavaScript Assets

--- application/views/V_Homepage.php
+++ application/views/V_Homepage.php
@@ -973,14 +973,14 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 
 	<!-- Scripts -->
-	<script src="js/jquery.min.js"></script> <!-- jQuery for Bootstrap's JavaScript plugins -->
-	<script src="js/popper.min.js"></script> <!-- Popper tooltip library for Bootstrap -->
-	<script src="js/bootstrap.min.js"></script> <!-- Bootstrap framework -->
-	<script src="js/jquery.easing.min.js"></script> <!-- jQuery Easing for smooth scrolling between anchors -->
-	<script src="js/swiper.min.js"></script> <!-- Swiper for image and text sliders -->
-	<script src="js/jquery.magnific-popup.js"></script> <!-- Magnific Popup for lightboxes -->
-	<script src="js/validator.min.js"></script> <!-- Validator.js - Bootstrap plugin that validates forms -->
-	<script src="js/scripts.js"></script> <!-- Custom scripts -->
+	<script src="<?= base_url('assets/homeassets/js/jquery.min.js') ?>"></script> <!-- jQuery for Bootstrap's JavaScript plugins -->
+	<script src="<?= base_url('assets/homeassets/js/popper.min.js') ?>"></script> <!-- Popper tooltip library for Bootstrap -->
+	<script src="<?= base_url('assets/homeassets/js/bootstrap.min.js') ?>"></script> <!-- Bootstrap framework -->
+	<script src="<?= base_url('assets/homeassets/js/jquery.easing.min.js') ?>"></script> <!-- jQuery Easing for smooth scrolling between anchors -->
+	<script src="<?= base_url('assets/homeassets/js/swiper.min.js') ?>"></script> <!-- Swiper for image and text sliders -->
+	<script src="<?= base_url('assets/homeassets/js/jquery.magnific-popup.js') ?>"></script> <!-- Magnific Popup for lightboxes -->
+	<script src="<?= base_url('assets/homeassets/js/validator.min.js') ?>"></script> <!-- Validator.js - Bootstrap plugin that validates forms -->
+	<script src="<?= base_url('assets/homeassets/js/scripts.js') ?>"></script> <!-- Custom scripts -->
 </body>
 
 </html>
