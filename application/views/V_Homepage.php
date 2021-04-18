6df8b270d6da87a6372faf33bd29be26ed6d94bf
Rafi Antareza Putra <1108rap@gmail.com>
2021-04-18 17:24:51 +0700

    Add Style with CSS and head content

--- application/views/V_Homepage.php
+++ application/views/V_Homepage.php
@@ -9,15 +9,19 @@ defined('BASEPATH') or exit('No direct script access allowed');
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta name="author" content="">
-	<meta name="keywords" content="">
-	<meta name="description" content="">
+	<meta name="author" content="Rafi Antareza Putra">
+	<meta name="keywords" content="Outsourcing Application">
+	<meta name="description" content="Management application for outsourcing system">
 	<!-- End of SEO Meta Tags -->
 
 	<!-- Icon n' Title -->
 	<link rel="icon" type="image/png" href="">
 	<title>Antime Outsourcing</title>
 	<!-- End of Icon n' Title -->
+
+	<!-- Style n' CSS -->
+	<link rel="stylesheet" href="<?= base_url('assets/css/antisme.css') ?>">
+	<!-- End of Style n' CSS -->
 </head>
 
 <body>
