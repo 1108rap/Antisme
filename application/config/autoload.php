bae8d4314bd45613f19f5b60cc7c6bff7e42447b
Rafi Antareza Putra <1108rap@gmail.com>
2021-04-18 19:09:56 +0700

    Add Models

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -132,4 +132,4 @@ $autoload['language'] = array();
 |
 |	$autoload['model'] = array('first_model' => 'first');
 */
-$autoload['model'] = array();
+$autoload['model'] = array('M_Create', 'M_Read', 'M_Update', 'M_Delete');
