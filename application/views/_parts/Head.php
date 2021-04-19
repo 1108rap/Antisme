93879f93d86f01fa825965ccd75c9b236be831e4
Rafi Antareza Putra <1108rap@gmail.com>
2021-04-19 20:22:34 +0700

    partials head and add script file

---
+++ application/views/_parts/Head.php
@@ -0,0 +1,21 @@
+<!-- SEO Meta Tags -->
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="author" content="Rafi Antareza Putra">
+<meta name="keywords" content="Outsourcing Application">
+<meta name="description" content="Management application for outsourcing system">
+<!-- End of SEO Meta Tags -->
+
+<!-- Icon n' Title -->
+<link rel="icon" type="image/png" href="<?= base_url('assets/img/favicon.png') ?>">
+<title>Antime Outsourcing</title>
+<!-- End of Icon n' Title -->
+
+<!-- Style n' CSS -->
+<link rel="stylesheet" href="<?= base_url('assets/css/antisme.css') ?>">
+<!-- End of Style n' CSS -->
+
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+<!-- End of Bootstrap CSS -->
